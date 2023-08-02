a5d8deba9a7d803a3238051003c555b83a1565b1
sky121016 <sky121016@naver.com>
2023-08-02 20:34:12 +0900

    Add S4-S5

---
+++ Slide/S4.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    let slider = document.querySelectorAll(".slider");
+
+    let currentIndex = 0;
+    slider[0].style.opacity = "1";
+
+    setInterval(()=>{
+        let nextIndex = (currentIndex + 1) % slider.length;
+
+        slider[currentIndex].style.opacity = "0";
+        slider[nextIndex].style.opacity = "1";
+
+        currentIndex = nextIndex;
+    },3000);
+}
---
+++ Slide/S5.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+    let sliderWrap = document.querySelector(".sliderWrap");
+    let slider = document.querySelectorAll(".slider");
+    const sliderClone = sliderWrap.firstElementChild.cloneNode(true);
+
+    sliderWrap.appendChild(sliderClone);
+
+    let currentIndex = 0;
+
+    setInterval(() => {
+        currentIndex++;
+        sliderWrap.style.marginLeft = -currentIndex * 100 + "%";
+        sliderWrap.style.transition = "all 0.6s";
+
+        if(currentIndex == 3){
+            setTimeout(()=>{
+                sliderWrap.style.marginLeft = "0";
+                sliderWrap.style.transition = "0s";
+                currentIndex = 0;
+            },700);
+        }
+
+    }, 3000);
+};
