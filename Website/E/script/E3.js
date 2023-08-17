58e21cc7bbc16910b4530627219f214e16b343e5
sky121016 <sky121016@naver.com>
2023-08-17 13:11:55 +0900

    Add E3 완성

---
+++ Website/E/script/E3.js
@@ -0,0 +1,48 @@
+window.onload = function(){
+    // nav
+    let navList = document.querySelectorAll(".nav > ul > li");
+    navList.forEach(nav => {
+        nav.addEventListener("mouseover", function(){
+            this.querySelector(".subMenu").style.height = "158px";
+        });
+        nav.addEventListener("mouseout", function(){
+            this.querySelector(".subMenu").style.height = "0";
+        });
+    });
+
+
+    // slider
+    let sliderWrap = document.querySelector(".sliderWrap");
+    let sliderClone = sliderWrap.firstElementChild.cloneNode(true);
+    sliderWrap.appendChild(sliderClone);
+    let currentIndex = 0;
+
+    setInterval(() => {
+        currentIndex++;
+        sliderWrap.style.marginTop = -currentIndex * 700 + "px";
+        sliderWrap.style.transition = "all 0.6s";
+
+        if(currentIndex == 3){
+            setTimeout(() => {
+                
+                sliderWrap.style.marginTop = "0";
+                sliderWrap.style.transition = "0s";
+                currentIndex = 0;
+            }, 700);
+        }
+    }, 3000);
+
+
+
+    // popup
+    let popup = document.querySelector(".popup");
+    let popupBtn = document.querySelector(".popup-btn");
+    let popupClose = document.querySelector(".popup-close");
+
+    popupBtn.addEventListener("click",function(){
+        popup.style.display = "block";
+    });
+    popupClose.addEventListener("click",function(){
+        popup.style.display = "none";
+    });
+}
